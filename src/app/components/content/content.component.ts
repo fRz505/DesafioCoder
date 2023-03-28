df9eb61a72fe986e282f851d278194bdb8b7c73b
Franco <francomarquillas@gmail.com>
2023-03-28 00:08:27 -0300

    changes

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-type Alumno = Array<{ id: number; name: string, lastName: string; }>
+type Alumno = { id: number; name: string, lastName: string; date: Date}
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  alumnos: Alumno = [
-    { id: 1, name: "Carlos", lastName: 'Sanchez' },
-    { id: 2, name: "Javier", lastName: 'Torres' },
-    { id: 3, name: "Franco", lastName: 'Marquillas' },
-    { id: 4, name: "Ariel", lastName: 'Gonzalez' },
-    { id: 5, name: "Lucas", lastName: 'Gutierrez' }
+
+  alumnos: Alumno[] = [
+    { id: 1, name: "Carlos", lastName: 'Sanchez', date: new Date() },
+    { id: 2, name: "Javier", lastName: 'Torres' , date: new Date() },
+    { id: 3, name: "Franco", lastName: 'Marquillas' , date: new Date() },
+    { id: 4, name: "Ariel", lastName: 'Gonzalez' , date: new Date() },
+    { id: 5, name: "Lucas", lastName: 'Gutierrez' , date: new Date() }
   ];
+
   constructor() { }
   ngOnInit() {
   }
